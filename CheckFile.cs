75bdde2b0d1207f75baf9cc8b5097837cf87566c
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2023-07-14 15:55:34 +0100

    Added project to test library functionality within a console

--- CheckFile.cs
+++ CheckFile.cs
@@ -1,26 +1,42 @@
-﻿namespace SHA256_Checksum
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHA256_Checksum
 {
     public class CheckFile
     {
         public string FilePath { get; set; }
-        private string? Checksum;
+        public string Checksum { get; private set; }
 
         public CheckFile(string filePath)
         {
             FilePath = filePath;
+            Checksum = GenerateChecksum();
         }
 
-        public void GenerateChecksum()
+        private string GenerateChecksum()
         {
+            if(File.Exists(FilePath))
+            {
+                using(SHA256 sha = SHA256.Create())
+                {
+                    byte[] bytes = sha.ComputeHash(File.ReadAllBytes(FilePath));
+                    StringBuilder sb = new StringBuilder();
 
-        }
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        sb.Append($"{bytes[i]:x2}");
+                    }
 
-        public string GetFileChecksum()
-        {
-            if (Checksum != null)
-                return Checksum;
-
-            return "";
+                    return sb.ToString();
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
         }
     }
 }
