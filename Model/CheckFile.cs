184000026d3f4ae640f48dafdef51cc1fa54c97d
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2023-07-14 15:19:36 +0100

    Added members for CheckFile class

--- Model/CheckFile.cs
+++ Model/CheckFile.cs
@@ -2,6 +2,25 @@
 {
     public class CheckFile
     {
+        public string FilePath { get; set; }
+        private string? Checksum;
 
+        public CheckFile(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void GenerateChecksum()
+        {
+
+        }
+
+        public string GetFileChecksum()
+        {
+            if(Checksum != null)
+                return Checksum;
+
+            return "";
+        }
     }
 }
