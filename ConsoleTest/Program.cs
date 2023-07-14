b8d7459c47caa7216757a31671394a670fce00cd
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2023-07-14 15:56:14 +0100

    Added basic console functionality code

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SHA256_Checksum;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Not enough arguments");
+    Console.WriteLine("\nCorrect usage\n---------\nArgument 1: File a\nArgument 2: File B");
+    return;
+}
+else
+{
+    CheckFile a = new CheckFile(args[0]);
+    CheckFile b = new CheckFile(args[1]);
+
+
+    if (a.Checksum == b.Checksum)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("File hashes match");
+        Console.WriteLine($"File hash: {a.Checksum}");
+        Console.ForegroundColor = ConsoleColor.White;
+
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("File hashes do not match");
+        Console.WriteLine($"File A Hash: {a.Checksum}");
+        Console.WriteLine($"File B Hash: {b.Checksum}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+}
